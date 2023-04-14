8af593edf2992493f21f2062ddaf89ee6c667b18
Paul Robson <paul@robsons.org.uk>
2023-04-14 15:52:00 +0100

    Update

--- source/modules/ifloat32/float/integerdown.asm
+++ source/modules/ifloat32/float/integerdown.asm
@@ -20,6 +20,7 @@
 
 FloatIntegerPartDown:
 		pha
+		phy
 		;
 		lda 	NSExponent,x 				; is it integer already ?
 		beq 	_FIPExit 					; if so do nothing
@@ -28,21 +29,36 @@ FloatIntegerPartDown:
 		;
 		jsr 	FloatNormalise 				; normalise
 		beq 	_FIPZero 					; normalised to zero, exit zero
+		ldy 	#0 							; reset the count of bits.
 		;
 _FIPShift:
 		lda 	NSExponent,x 				; if Exponent >= 0 exit.
-		bpl 	_FIPCheckZero		 		
+		bpl 	_FIPCheckDown
 
 		jsr 	FloatShiftRight 			; shift mantissa right
+		bcc 	_FIPNoFrac 					; shifted a zero out ?
+		iny
+_FIPNoFrac:		
 		inc 	NSExponent,x 				; bump exponent 
 		bra 	_FIPShift
 
+_FIPCheckDown:
+		cpy 	#0 							; were there any fractional bits.
+		beq 	_FIPCheckZero
+		bit 	NSStatus,x 					; +ve
+		bpl 	_FIPCheckZero 
+		inx 								; -ve so round *down*.
+		lda 	#1
+		jsr 	FloatSetByte
+		jsr 	FloatNegate
+		jsr 	FloatAdd
 _FIPCheckZero:
 		jsr 	FloatIsZero 				; avoid -0 problem
 		bne 	_FIPExit 					; set to zero if mantissa zero.		
 _FIPZero:
 		jsr 	FloatSetZero
 _FIPExit:
+		ply
 		pla
 		rts		
 
