32ea6fbbc699156cfc7ec88934cbe2d99d927789
Paul Robson <paul@robsons.org.uk>
2023-10-14 08:55:31 +0100

    Tweaks

--- source/unit-tests/compiler-runtime/testing/testing.asm
+++ source/unit-tests/compiler-runtime/testing/testing.asm
@@ -2,7 +2,7 @@
 ; ************************************************************************************************
 ;
 ;		Name:		testing.asm
-;		Purpose:	Code End Marker
+;		Purpose:	Basic testing for runtime
 ;		Created:	11th April 2023
 ;		Reviewed: 	No
 ;		Author:		Paul Robson (paul@robsons.org.uk)
@@ -12,11 +12,27 @@
 
 		.section code
 
-EndProgram:
-		.binary "../build/test.prg"
+StartWorkSpace = $8000
+EndWorkspace = $9F00
 
+WrapperBoot:	
+		ldx 	#APIDesc & $FF
+		ldy 	#APIDesc >> 8
+		jsr 	StartCompiler
+_WBError: 									; stop on error
+		bcs 	_WBError
+		jmp 	$FFFF
+
+APIDesc:
+		.word 	TestAPI 					; the testing API.
+		.byte 	StartWorkSpace >> 8 		; start of workspace for compiler
+		.byte 	EndWorkspace >> 8 			; end of workspace for compiler
+											; this example is 8000-9EFF.
 		.send code
 
+		.include "../../../compiler/testing/api/api.asm"
+		.include "../../../compiler/testing/api/line.asm"
+		.include "../../../compiler/testing/api/save.asm"
 
 ; ************************************************************************************************
 ;
@@ -28,3 +44,4 @@ EndProgram:
 ;		==== 			=====
 ;
 ; ************************************************************************************************
+
